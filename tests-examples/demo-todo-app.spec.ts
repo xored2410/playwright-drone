8b3cf671da118c7dc929582f23ca4adcfbeca986
xored <edineminovic1024@gmail.com>
2024-05-19 16:18:06 +0200

    adding local project to the repo

---
+++ tests-examples/demo-todo-app.spec.ts
@@ -0,0 +1,10 @@
+import { test } from '@playwright/test';
+import ENV from '../utils/env';
+
+test('Diverse environments', async ({}) => {
+
+  console.log(`This is base URL: ${ENV.BASE_URL}`);
+  console.log(`This is username: ${ENV.USERNAME}`);
+  console.log(`This is password: ${ENV.PASSWORD}`);
+
+});
---
+++ utils/globalSetup.ts
@@ -0,0 +1,19 @@
+import { FullConfig } from "@playwright/test";
+import dotenv from "dotenv"
+
+async function globalSetup(config: FullConfig) {
+
+    try {
+        if (process.env.test_env) {
+            dotenv.config({
+                path: `.env.${process.env.test_env}`,
+                override: true
+            });
+        }
+    } catch (error) {
+        console.error("Error loading environment variables:", error);
+    }
+
+   
+}
+export default globalSetup;
