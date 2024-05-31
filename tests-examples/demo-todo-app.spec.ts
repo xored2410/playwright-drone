a1f96133d6b562cbed7b27a627e87332769b78d9
xored <edineminovic1024@gmail.com>
2024-05-31 22:40:12 +0200

    get some data

--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -3,8 +3,15 @@ import ENV from '../utils/env';
 
 test('Diverse environments', async ({}) => {
 
+  const c = require('./package.json');
+
+
+
   console.log(`This is base URL: ${ENV.BASE_URL}`);
   console.log(`This is username: ${ENV.USERNAME}`);
   console.log(`This is password: ${ENV.PASSWORD}`);
+
+  console.log('scripts:', c.scripts);
+
 
 });
