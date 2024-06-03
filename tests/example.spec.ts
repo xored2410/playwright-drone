360e96ba70a8e5af240f908f7b220dd6a7685e00
xored <edineminovic1024@gmail.com>
2024-06-03 11:26:14 +0200

    params at the start of command

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,14 +3,11 @@ import ENV from '../utils/env';
 
 test('Diverse environments', async ({}) => {
 
-  const c = require('./package.json');
 
 
   console.log(`This is base URL: ${ENV.BASE_URL}`);
   console.log(`This is username: ${ENV.USERNAME}`);
   console.log(`This is password: ${ENV.PASSWORD}`);
 
-
-  console.log('scripts:', c.scripts);
 
 });
